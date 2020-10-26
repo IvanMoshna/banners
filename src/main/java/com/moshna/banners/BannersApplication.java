852727b86ca2e7a78399533870769c59952d94c1
Ivan Moshna <ittifock@gmail.com>
2020-10-26 15:04:54 +0700

    Initial commit

---
+++ src/main/java/com/moshna/banners/BannersApplication.java
@@ -0,0 +1,13 @@
+package com.moshna.banners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannersApplication.class, args);
+	}
+
+}
