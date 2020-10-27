a5c8dad7716972d7d7fa702ef9c1e0c1d1aea4ca
Ivan Moshna <ittifock@gmail.com>
2020-10-27 15:02:56 +0700

    add required in htmls, trying to get categoryID

--- src/main/java/com/moshna/banners/controllers/BannerController.java
+++ src/main/java/com/moshna/banners/controllers/BannerController.java
@@ -3,30 +3,51 @@ package com.moshna.banners.controllers;
 import com.moshna.banners.models.Banner;
 import com.moshna.banners.models.Category;
 import com.moshna.banners.repo.BannerRepository;
+import com.moshna.banners.repo.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class BannerController {
 
     @Autowired
     private BannerRepository bannerRepository;
+    private CategoryRepository categoryRepository;
+
+    public BannerController(BannerRepository bannerRepository, CategoryRepository categoryRepository) {
+        this.bannerRepository = bannerRepository;
+        this.categoryRepository = categoryRepository;
+    }
 
     @GetMapping("/banner")
     public String categoryMain(Model model) {
+        Iterable<Category> categories = categoryRepository.findAll();
+        Collection<Category> categoryList = new ArrayList<>();
+        for (Category item : categories) {
+            categoryList.add(new Category(item.getId(), item.getName(), item.getReq_name(), item.isDeleted()));
+        }
+
+        model.addAttribute("categories", categoryList);
         model.addAttribute("banner", "Banners");
         return "banner-main";
     }
 
     @PostMapping("/banner")
-    public String bannerPostAdd(@RequestParam String name, @RequestParam double price, @RequestParam Long categoryID,
+    public String bannerPostAdd(@RequestParam String name, @RequestParam double price, @ModelAttribute Category category,
                                 @RequestParam String text, Model model) {
-        Banner banner = new Banner(name, price, categoryID, text);
+
+        Banner banner = new Banner(name, price, category.getId(), text);
         bannerRepository.save(banner);
+
         return "redirect:/";
     }
 
